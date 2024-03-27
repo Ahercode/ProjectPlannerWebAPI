862641c433a27b9213895a5843262ac4789e181f
Ahercode <philipkelly407@gmail.com>
2024-03-27 16:49:20 +0000

    added interfaces for some of the Entities

---
+++ ProjectFinance.Infrastructure/Repositories/Interfaces/IActivityRepository.cs
@@ -0,0 +1,8 @@
+using System.Diagnostics;
+
+namespace ProjectFinance.Infrastructure.Repositories.Interfaces;
+
+public interface IActivityRepository: IGenericRepository<Activity>
+{
+    
+}
---
+++ ProjectFinance.Infrastructure/Repositories/Interfaces/ICostCategoryRepository.cs
@@ -0,0 +1,8 @@
+using ProjectFinance.Domain.Entities;
+
+namespace ProjectFinance.Infrastructure.Repositories.Interfaces;
+
+public interface ICostCategoryRepository: IGenericRepository<CostCategory>
+{
+    
+}
