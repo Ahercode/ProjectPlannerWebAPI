90415be4e64bedc1c04a252fae01d39908a706e4
Ahercode <philipkelly407@gmail.com>
2024-03-27 17:39:01 +0000

    Added unit of work

---
+++ ProjectFinance.Infrastructure/Repositories/Interfaces/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,28 @@
+namespace ProjectFinance.Infrastructure.Repositories.Interfaces.UnitOfWork;
+
+public interface IUnitOfWork
+{
+    IActivityRepository Activities { get; }
+    IFinanceOptionRepository FinanceOptions { get; }
+    IBankRepository Banks { get; }
+    IClientRepository Clients { get; }
+    ICostCategoryRepository CostCategories { get; }
+    ICostDetailRepository CostDetails { get; }
+    ICurrencyRepository Currencies { get; }
+    IFinanceOptionScheduleRepository FinanceOptionSchedules { get; }
+    IInvoiceRepository Invoices { get; }
+    IPaymentRepository Payments { get; }
+    IPOPayScheduleRepository POPaySchedules { get; }
+    IProjectRepository Projects { get; }
+    IProjectCategoryRepository ProjectCategories { get; }
+    IProjectActivityRepository ProjectActivities { get; }
+    IProjectActivityCostRepository ProjectActivityCosts { get; }
+    IProjectScheduleRepository ProjectSchedules { get; }
+    IProjectTypeRepository ProjectTypes { get; }
+    IPurchaseOrderRepository PurchaseOrders { get; }
+    ISatffRepository Staffs { get; }
+    ISupplierRepository Suppliers { get; }
+    
+    
+    Task CompleteAsync();
+}
